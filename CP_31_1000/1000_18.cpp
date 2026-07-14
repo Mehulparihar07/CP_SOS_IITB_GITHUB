b4a49f576659785a56209795069691a4ed4875a8
Mehulparihar07 <pariharmehul13@gmail.com>
2026-07-14 13:39:11 +0530

    ms

--- CP_31_1000/1000_18.cpp
+++ CP_31_1000/1000_18.cpp
@@ -1,7 +1,43 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(){
+    int t ;
+    cin >> t ;
+    while (t--)
+    {
+        long long w , h , k , x ;
+        cin >> w >> h ;
+        vector<long long> xi , xf , yi , yf ;
+        cin >> k ;
+        for (int i = 0 ; i < k ; i++)
+        {
+            cin >> x ;
+            xi.push_back(x) ;
+        }
+        cin >> k ;
+        for (int i = 0 ; i < k ; i++)
+        {
+            cin >> x ;
+            xf.push_back(x) ;
+        }
+        cin >> k ;
+        for (int i = 0 ; i < k ; i++)
+        {
+            cin >> x ;
+            yi.push_back(x) ;
+        }
+        cin >> k ;
+        for (int i = 0 ; i < k ; i++)
+        {
+            cin >> x ;
+            yf.push_back(x) ;
+        }
+        
+        
+    }
     
     return 0;
 }
