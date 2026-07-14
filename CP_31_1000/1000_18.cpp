351a9bc9d9ec7ad2e30d06e0d3e06c838d53457a
Mehulparihar07 <pariharmehul13@gmail.com>
2026-07-14 15:55:58 +0530

    ms

--- CP_31_1000/1000_18.cpp
+++ CP_31_1000/1000_18.cpp
@@ -3,60 +3,49 @@
 #include<algorithm>
 using namespace std;
 
-int main(){
-    int t ;
-    cin >> t ;
-    while (t--)
-    {
-        long long w , h , k , x ;
-        cin >> w >> h ;
-        vector<pair<long long , long long>> xi , xf , yi , yf ;
-        cin >> k ;
-        for (int i = 0 ; i < k ; i++)
-        {
-            cin >> x ;
-            xi.push_back({x,0}) ;
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long w, h, k, x;
+        cin >> w >> h;
+
+        vector<pair<long long, long long>> xi, xf, yi, yf;
+
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            cin >> x;
+            xi.push_back({x, 0});
         }
-        cin >> k ;
-        for (int i = 0 ; i < k ; i++)
-        {
-            cin >> x ;
-            xf.push_back({x,h}) ;
+
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            cin >> x;
+            xf.push_back({x, h});
         }
-        cin >> k ;
-        for (int i = 0 ; i < k ; i++)
-        {
-            cin >> x ;
-            yi.push_back({0,x}) ;
+
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            cin >> x;
+            yi.push_back({0, x});
         }
-        cin >> k ;
-        for (int i = 0 ; i < k ; i++)
-        {
-            cin >> x ;
-            yf.push_back({w,x}) ;
+
+        cin >> k;
+        for (int i = 0; i < k; i++) {
+            cin >> x;
+            yf.push_back({w, x});
         }
-        long long base_max = xi[xi.size() - 1].first - xi[0].first ;
-        bool xin = true , yin = false , xfn = false , yfn = false ;
-        if((xf[xf.size() - 1].first - xf[0].first) > base_max){
-            base_max = xf[xf.size() - 1].first - xf[0].first ;
-            xfn = true ; yfn = false ;
-            xin = false ;   yin = false ;
-        }
-         if((yi[yi.size() - 1].second - yi[0].second) > base_max){
-            base_max = yi[yi.size() - 1].second - yi[0].second ;
-            yin = true ; xfn = false ;
-            xin = false ; yfn = false ;
-        }
-         if((yf[yf.size() - 1].second - yf[0].second) > base_max){
-            base_max = yf[xf.size() - 1].second - yf[0].second ;
-            yfn = true ; yin = false ;
-            xin = false ; xfn = false ;
-        }
-        
-        
-        
-        
+
+        long long ans_c1 = 0, ans_c2 = 0, ans_c3 = 0, ans_c4 = 0;
+
+        ans_c1 = (xi.back().first - xi.front().first) * h;
+        ans_c2 = (xf.back().first - xf.front().first) * h;
+        ans_c3 = (yi.back().second - yi.front().second) * w;
+        ans_c4 = (yf.back().second - yf.front().second) * w;
+
+        cout << max(max(ans_c1, ans_c2), max(ans_c3, ans_c4)) << "\n";
     }
-    
+
     return 0;
 }
