faa5f78e17dca0e0fad9e5e2126895e9e3176339
Mehulparihar07 <pariharmehul13@gmail.com>
2026-07-14 13:58:52 +0530

    ms

--- CP_31_1000/1000_18.cpp
+++ CP_31_1000/1000_18.cpp
@@ -10,31 +10,36 @@ int main(){
     {
         long long w , h , k , x ;
         cin >> w >> h ;
-        vector<long long> xi , xf , yi , yf ;
+        vector<pair<long long , long long>> xi , xf , yi , yf ;
         cin >> k ;
         for (int i = 0 ; i < k ; i++)
         {
             cin >> x ;
-            xi.push_back(x) ;
+            xi.push_back({x,0}) ;
         }
         cin >> k ;
         for (int i = 0 ; i < k ; i++)
         {
             cin >> x ;
-            xf.push_back(x) ;
+            xf.push_back({x,h}) ;
         }
         cin >> k ;
         for (int i = 0 ; i < k ; i++)
         {
             cin >> x ;
-            yi.push_back(x) ;
+            yi.push_back({0,x}) ;
         }
         cin >> k ;
         for (int i = 0 ; i < k ; i++)
         {
             cin >> x ;
-            yf.push_back(x) ;
+            yf.push_back({w,x}) ;
         }
+        for (int i = 0 ; i < 4 ; i++)
+        {
+            /* code */
+        }
+        
         
         
     }
