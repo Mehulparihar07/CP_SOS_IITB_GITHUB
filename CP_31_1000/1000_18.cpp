3911bf2e53fd89866bbc1358c403e70f125a07ed
Mehulparihar07 <pariharmehul13@gmail.com>
2026-07-14 15:18:03 +0530

    mk

--- CP_31_1000/1000_18.cpp
+++ CP_31_1000/1000_18.cpp
@@ -35,10 +35,24 @@ int main(){
             cin >> x ;
             yf.push_back({w,x}) ;
         }
-        for (int i = 0 ; i < 4 ; i++)
-        {
-            /* code */
+        long long base_max = xi[xi.size() - 1].first - xi[0].first ;
+        bool xin = true , yin = false , xfn = false , yfn = false ;
+        if((xf[xf.size() - 1].first - xf[0].first) > base_max){
+            base_max = xf[xf.size() - 1].first - xf[0].first ;
+            xfn = true ; yfn = false ;
+            xin = false ;   yin = false ;
         }
+         if((yi[yi.size() - 1].second - yi[0].second) > base_max){
+            base_max = yi[yi.size() - 1].second - yi[0].second ;
+            yin = true ; xfn = false ;
+            xin = false ; yfn = false ;
+        }
+         if((yf[yf.size() - 1].second - yf[0].second) > base_max){
+            base_max = yf[xf.size() - 1].second - yf[0].second ;
+            yfn = true ; yin = false ;
+            xin = false ; xfn = false ;
+        }
+        
         
         
         
